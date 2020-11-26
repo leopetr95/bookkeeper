8d5c9bf8db5a6ec3d4467a315370a896d0c9e9b3
leopetr95 <leonardo_petraglia@hotmail.it>
2020-11-26 11:14:27 +0100

    Commit

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieStatusEnclosedTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieStatusEnclosedTest.java
@@ -24,6 +24,8 @@ import static org.mockito.Mockito.when;
 @RunWith(Enclosed.class)
 public class BookieStatusEnclosedTest {
 
+    //prova
+
     @BeforeClass
     public static void createDirectory(){
 
